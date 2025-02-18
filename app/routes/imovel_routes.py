439d896cd3aad92e44ea36a2c7b2e5949c1f1f8f
Wesley00s <wesley300rodrigues@gmail.com>
2025-02-18 11:14:25 -0300

    update: change 'imoveis' route name to 'create' and 'list' properties

--- app/routes/imovel_routes.py
+++ app/routes/imovel_routes.py
@@ -6,10 +6,10 @@ from app.core.database import get_db
 
 router = APIRouter()
 
-@router.post("/imoveis", response_model=ImovelResponse)
+@router.post("/create", response_model=ImovelResponse)
 def add_imovel(imovel: ImovelCreate, db: Session = Depends(get_db)):
     return create_imovel(db, imovel)
 
-@router.get("/imoveis", response_model=list[ImovelResponse])
+@router.get("/list", response_model=list[ImovelResponse])
 def list_imoveis(db: Session = Depends(get_db)):
     return get_imoveis(db)
