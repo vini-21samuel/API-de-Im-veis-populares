9e0d6ece69b4dda75da0c92b9c8178318029298f
Wesley00s <wesley300rodrigues@gmail.com>
2025-02-19 18:11:09 -0300

    refactor: remove unnecessary schemas

--- app/schemas/transacao_schema.py
+++ app/schemas/transacao_schema.py
@@ -1,17 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+
 class TransacaoBase(BaseModel):
     imovel_id: int
-    comprador_id: int
-    valor: float
-
-class TransacaoCreate(TransacaoBase):
-    pass
-
-class TransacaoResponse(TransacaoBase):
-    id: int
-    data: datetime
-
-    class Config:
-        from_attributes = True
+    user_id: int
