facc117dd9adbab1409e8bdbe7d4899497e51141
Wesley00s <wesley300rodrigues@gmail.com>
2025-02-19 18:11:33 -0300

    refactor: refactor user base model, add is admin field

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,24 +1,18 @@
 from pydantic import BaseModel
 
-# Esquema base para o usuário (dados comuns em várias operações)
 class UserBase(BaseModel):
     nome: str
     email: str
+    is_admin: bool = False
 
-# Esquema para criação de usuário
 class UserCreate(UserBase):
-    senha: str  # Senha será recebida em texto simples (será hashada ao criar o usuário)
-    telefone: str  # Adicionado campo telefone
+    senha: str
+    telefone: str
 
-# Esquema para resposta do usuário
 class UserResponse(UserBase):
     id: int
-    telefone: str  # Adicionado campo telefone
+    telefone: str
 
-    class Config:
-        from_attributes = True  # Atualizado para Pydantic V2
-
-# Novo esquema para login, apenas com email e senha
 class UserLogin(BaseModel):
     email: str
     senha: str
