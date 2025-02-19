efc340fecc71582704366621a9dcf3e4e9eccb67
Wesley00s <wesley300rodrigues@gmail.com>
2025-02-19 18:13:53 -0300

    refactor: refactor return methods

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,12 +1,58 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
-from app.schemas.user_schema import UserCreate
+from app.services.auth_service import hash_password
 
-def create_user(db: Session, user):
+
+def create_user_repository(db: Session, user):
+    try :
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+        return {
+            "status": "success",
+            "data": user
+        }
+    except Exception as e:
+        db.rollback()
+        return None
+
+def get_user_by_id_repository(db: Session, id: int):
+    user = db.query(User).filter(User.id == id).first()
+
+    if user is None:
+        raise None
+
+    return {
+        "status": "success",
+        "data": user
+    }
+
+def get_user_by_email_repository(db: Session, email: str):
+    user = db.query(User).filter(User.email == email).first()
+
+    if user is None:
+        raise None
+
+    return {
+        "status": "success",
+        "data": user
+    }
+
+
+def recover_password_repository(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        return None
+
+    hashed_password = hash_password(password)
+    user.senha = hashed_password
+
     db.add(user)
     db.commit()
     db.refresh(user)
-    return user
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+    return {
+        "status": "success",
+        "user": user
+    }
