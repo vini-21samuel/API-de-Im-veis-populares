637ca7bd11d9830fe3f84f95b9ec4dfb6ca51cdc
Wesley00s <wesley300rodrigues@gmail.com>
2025-02-19 18:09:24 -0300

    chore: remove comments

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -3,16 +3,18 @@ from jose import jwt
 from passlib.context import CryptContext
 from app.config import settings
 from sqlalchemy.orm import Session
-from app.models.user import User  # Corrigindo a importação
-from app.repositories.user_repository import get_user_by_email  # Mantendo a importação correta
+from app.models.user import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str) -> str:
+
+def hash_password(password: str):
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
+
+def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
 
 def create_access_token(data: dict) -> str:
     expire = datetime.now() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -20,11 +22,10 @@ def create_access_token(data: dict) -> str:
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
 
+
 def authenticate_user(db: Session, email: str, senha: str):
-    # Buscar o usuário no banco de dados pelo email
     user = db.query(User).filter(User.email == email).first()
-    
-    # Verificar se o usuário foi encontrado e se a senha é válida
+
     if user and verify_password(senha, user.senha):
         return user
     return None
