450c3ad05cbd8559c200e3a296c432d7433a2b9d
Wesley00s <wesley300rodrigues@gmail.com>
2025-02-19 18:09:13 -0300

    chore: remove comments

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ JWT_SECRET = os.getenv("JWT_SECRET")
 JWT_ALGORITH = os.getenv("JWT_ALGORITH")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
-# Classe de configurações
+
 class Settings(BaseSettings):
     POSTGRES_PORT: int = POSTGRES_PORT
     POSTGRES_PASSWORD: str = POSTGRES_PASSWORD
@@ -32,23 +32,20 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = ACCESS_TOKEN_EXPIRE_MINUTES
 
     class Config:
-        env_file = "./.env"  # Arquivo de variáveis de ambiente
+        env_file = "./.env"
 
-# Instancia a classe de configurações
+
 settings = Settings()
 
-# Função para criar o token de acesso
+
 def create_access_token(data: dict, expires_delta: timedelta = None):
-    # Definir o tempo de expiração
     if expires_delta:
         expire = datetime.now() + expires_delta
     else:
         expire = datetime.now() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
-    # Criar o payload
     to_encode = data.copy()
     to_encode.update({"exp": expire})
-    
-    # Gerar o token JWT usando as configurações
+
     encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
     return encoded_jwt
