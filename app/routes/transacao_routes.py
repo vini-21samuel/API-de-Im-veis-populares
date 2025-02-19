16f4ee260aa23cba660cefe9309c23687256d8c7
Wesley00s <wesley300rodrigues@gmail.com>
2025-02-19 18:12:55 -0300

    refactor: refactor transacao routes

--- app/routes/transacao_routes.py
+++ app/routes/transacao_routes.py
@@ -1,16 +1,25 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette.status import HTTP_400_BAD_REQUEST
+
 from app.core.database import get_db
-from app.repositories.transacao_repository import create_transacao, list_transacoes
-from app.schemas.transacao_schema import TransacaoCreate
+from app.repositories.transacao_repository import create_transacao_repository, list_transacoes_repository
+from app.schemas.transacao_schema import TransacaoBase
 
 router = APIRouter()
 
-@router.post("/locacao")
-@router.post("/venda")
-def registrar_transacao(transacao: TransacaoCreate, db: Session = Depends(get_db)):
-    return create_transacao(db, transacao)
 
-@router.get("/listar")
+@router.post("/create")
+def registrar_transacao(transacao: TransacaoBase, db: Session = Depends(get_db)):
+    transacao_created = create_transacao_repository(db, transacao)
+
+    if transacao_created is None:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
+                            detail="Erro na requisição, verifique os dados informados")
+
+    return transacao_created
+
+
+@router.get("/list")
 def listar_transacoes(db: Session = Depends(get_db)):
-    return list_transacoes(db)
+    return list_transacoes_repository(db)
